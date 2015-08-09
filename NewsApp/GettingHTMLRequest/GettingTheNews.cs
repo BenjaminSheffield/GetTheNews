3943e635a1a6f879163248fb7bd00fa6601d646f
Benjamin Sheffield <benjaminsheffield@live.com>
2015-08-09 23:27:20 +1200

    initial commit

---
+++ NewsApp/GettingHTMLRequest/GettingTheNews.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace GettingHTMLRequest
+{
+    public class GettingTheNews
+    {
+        public List<string> gettingNewsItemsWebRequest()
+        {
+            var webget = new HtmlWeb();
+            //HtmlWeb.Load
+            WebRequest request = WebRequest.Create("http://i.stuff.co.nz");
+            WebResponse response = request.GetResponse();
+
+            var doc = webget.Load("http://i.stuff.co.nz");
+            //var doc = webrequest.Load()
+            List<string> newsHeadlines = null;
+            //HtmlNodeCollection testing = response.;
+            HtmlNodeCollection headlines = doc.DocumentNode.SelectNodes("//*[@id='mobContent']/div/div/div/h3/a");
+
+            foreach (var title in headlines)
+            {
+                newsHeadlines.Add(title.InnerHtml);
+            }
+            return newsHeadlines;
+        }
+    }
+}
---
+++ NewsApp/TheNews/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace TheNews
+{
+    public class Class1
+    {
+        public List<string> gettingNewsItemsWebRequest()
+        {
+            var webget = new HtmlWeb();
+            //HtmlWeb.Load
+            WebRequest request = WebRequest.Create("http://i.stuff.co.nz");
+            WebResponse response = request.GetResponse();
+
+            var doc = webget.Load("http://i.stuff.co.nz");
+            //var doc = webrequest.Load()
+            List<string> newsHeadlines = null;
+            //HtmlNodeCollection testing = response.;
+            HtmlNodeCollection headlines = doc.DocumentNode.SelectNodes("//*[@id='mobContent']/div/div/div/h3/a");
+
+            foreach (var title in headlines)
+            {
+                newsHeadlines.Add(title.InnerHtml);
+            }
+            return newsHeadlines;
+        }
+    }
+}
